8431d4615dad8c667fedcd6e6b90562890afe700
Александр <theones777@yandex.ru>
2022-12-29 10:37:40 +0300

    init

--- read_ids.py
+++ read_ids.py
@@ -2,6 +2,15 @@ import json
 import pprint
 
 
-with open('ids.json') as f:
-    a = json.load(f)
-pprint.pprint(a)
+def refactor_dict(d_path, suff):
+    with open(d_path) as f:
+        dict_list = json.load(f)
+    res_dict = {}
+    for el in dict_list:
+        res_dict[list(el.keys())[0]] = f'{suff}---id{list(el.values())[0][0]}'
+    pprint.pprint(dict_list)
+    print()
+    pprint.pprint(res_dict)
+
+
+refactor_dict('ids.json', 'geo')
